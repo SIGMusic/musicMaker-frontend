a20a534b5697237e89f9d0cdc3d69039e339d489
weikeng2 <42951918+weikeng2@users.noreply.github.com>
2019-03-06 16:55:31 -0600

    Basic functionality?

--- src/components/musicMakerHistory.js
+++ src/components/musicMakerHistory.js
@@ -3,28 +3,20 @@ import { Feed } from 'semantic-ui-react';
 
 const events = []
 
-function chordEventMaker(chord) {
-    const image = chordImage(chord);
-    const notes = chordNotes(chord);
-
-    events.push({image: image, extraText: notes})
+function chordEventMaker(chord, chordNotes) {
+    events.push({summary: chord, extraText: chordNotes});
 }
-
-function chordImage(chord) {
-    switch (chord) {
-        default:
-            return "../assets/C-chord.jpg";
-    }
-}
-
+/*
 function chordNotes(chord) {
-    switch (chord) {
-        default:
-            return "C, E, G";
-    }
+    var colonIndex = chord.indexOf(":");
+    var rootNote = chord.subString(0, colonIndex);
 }
+
+function chordScaleDegree(root, degree) {
+    return root;
+}
+*/
 
 const MusicMakerHistory = () => <Feed events={events} />
-
 
 export default MusicMakerHistory;
